af292193a3650ff84174f09d99e9cdb7a44862fb
Patrick Cardoso <patrickcardoso@Laptop-de-Patrick.local>
2025-04-17 00:18:12 -0300

    🧼 Corrigido workflow com fallback de instalação para 'schedule'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "975fca00d75babf4"; }
+/******/ 		__webpack_require__.h = function() { return "f0ffd26ab86764ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
