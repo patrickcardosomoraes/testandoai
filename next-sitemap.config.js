8ec27b627b1866f4126086f5eed36ec383dad9ba
Patrick Cardoso <patrickcardoso@Laptop-de-Patrick.local>
2025-04-17 08:45:11 -0300

    🗺️ Adiciona sitemap.xml e robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://testandoai.com.br',
-    generateRobotsTxt: true,
-  };
-  
+  siteUrl: 'https://testandoai.com.br', // substitua pelo seu domínio real
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+}
